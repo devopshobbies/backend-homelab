a57e71d0323deec4cd9723e4087a8dc26e5f00c9
Parsa asgari <parsa.asgari3264@gmail.com>
2025-03-06 10:04:27 +0000

    patch: added automatic admin creation on startup

--- fastapi_backend/main.py
+++ fastapi_backend/main.py
@@ -7,12 +7,25 @@ from contextlib import asynccontextmanager
 from piccolo.engine import engine_finder
 from fastapi_backend.piccolo_app import APP_CONFIG
 from fastapi_backend.routes import router_main
+from piccolo.apps.user.tables import BaseUser
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup Code
     engine = engine_finder()
     await engine.start_connection_pool()
+
+    # create admin user
+    print("checking if admin user_exsits")
+    admin_user = await BaseUser.select(BaseUser.username).where(BaseUser.username == 'admin')
+    if len(admin_user) == 0 :
+        print("created admin user.👍")
+        print("use the credentials below to access the admin panel:")
+        print("user: admin  ---  password: admin123456")
+        await BaseUser.create_user(username="admin", password="admin123456", active=True, superuser=True, admin=True)
+    else:
+        print("admin user exists. skipping admin user creation...")
+    
 
     yield
 
--- main.dev.py
+++ main.dev.py
@@ -2,4 +2,4 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("fastapi_backend_app:app", reload=False,
-                port=8990, host="0.0.0.0")
+                port=8000, host="0.0.0.0")
